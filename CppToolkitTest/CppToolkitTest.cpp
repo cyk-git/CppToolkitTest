39b4cb3b79ad524a6e9904603fb771f8edcfafd4
cyk-git <cyk_phy@mail.ustc.edu.cn>
2023-09-10 14:39:21 +0800

    chore(*):Init repo

---
+++ CppToolkitTest/CppToolkitTest.cpp
@@ -0,0 +1,9 @@
+﻿// CppToolkitTest.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+// 此项目用于常用C++工具库CppTookit的开发测试。CppTookit拥有独立的仓库，其作为一个子模块导入该仓库
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
